d308678e73cd5626f42a624fb6ec38972750feef
quangtung97 <quangtung29121997@gmail.com>
2018-05-19 13:17:44 +0700

    Boostrap Example

--- code/design/login/views.py
+++ code/design/login/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+class Tung:
+    name = "quang tung"
+    age = "21"
+
+
+def index(request):
+    numbers = [1, 2, 3, 4, 5]
+    name = "ta quang tung"
+    tung = Tung()
+
+    args = {"name": name, "numbers": numbers, "tung": tung}
+
+    return render(request, 'login/login.html', args)
