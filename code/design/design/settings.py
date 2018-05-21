f39db3f1f7bea72cf5a3116ea297c4b6908fd590
theGioiLa <theGioiLa@github.com>
2018-05-21 14:47:56 +0700

    final

--- code/design/design/settings.py
+++ code/design/design/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'design.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'design',
-        'USER': 'design',
-        'PASSWORD': 'quangtung97',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
